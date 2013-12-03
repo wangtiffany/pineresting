027ca03ed973132baaa0c0f048771793a849fdb3
Tiffany Wang <t.wang.jp@gmail.com>
2013-12-02 17:19:21 -0800

    customize bootstrap variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pineresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "menu" => "pages#menu"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
