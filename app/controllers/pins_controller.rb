d7b47bd909445b2bd27f7b6146f0d0cbcc63c2cd
Tiffany Wang <t.wang.jp@gmail.com>
2013-12-03 21:39:07 -0800

    pins changes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -17,33 +17,33 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
-
-      if @pin.save
-        redirect_to @pin, notice: 'Pin was successfully created.' 
-      else
-        render action: 'new' 
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.'
-      else
-        render action: 'edit'
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-      redirect_to pins_url
+    redirect_to pins_url
   end
 
   private
-
+    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
     end
