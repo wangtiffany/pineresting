a9f1dd010d5dbe975aeba6a88eceffb7277c057a
Tiffany Wang <t.wang.jp@gmail.com>
2013-12-06 10:18:42 -0800

    fixed again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Pineresting::Application.configure do
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-        :bucket => ENV['S3_BUCKET'],
+        :bucket => ENV['AWS_BUCKET'],
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
