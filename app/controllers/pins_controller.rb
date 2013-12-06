f367f1c2aecc2a7d8854e77cce5b83937c0ecb2d
Tiffany Wang <t.wang.jp@gmail.com>
2013-12-06 10:02:25 -0800

    fixed error

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,5 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -46,13 +44,12 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-        @pin = current_user.pins.find_by(id: params[:id])
-        redirect_to pins_path, notice: "not authorized to edit this pin" if @pin.nil?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
-      #redirect_to pins_path if @pin.nil?
     end
 end
