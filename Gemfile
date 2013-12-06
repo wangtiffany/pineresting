ed6d3581061a8c9b716e5f4b5c6d5095597f0903
Tiffany Wang <t.wang.jp@gmail.com>
2013-12-06 09:44:54 -0800

    add amazon s3 for paperclip uploads to heroku

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: '3'
 gem "devise", "~> 3.2.2"
 gem "paperclip", "~>3.0"
+gem "aws-sdk", "~> 1.29.1"
 
 group :development, :test do
 	# Use sqlite3 as the database for Active Record
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,16 @@ Pineresting::Application.configure do
   #note to set this to actual host
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+#This allows paperclip to upload images to amazon s3
+    # config/environments/production.rb
+    config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['S3_BUCKET_NAME'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
+
 
 end
