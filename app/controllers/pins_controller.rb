f65981f9c970408cad4f818277bd27cc3004a062
Tiffany Wang <t.wang.jp@gmail.com>
2013-12-07 08:58:16 -0800

    added pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,7 +2,7 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   def index
-    @pins = Pin.all.order("created_at DESC")
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 1)
   end
 
   def show
